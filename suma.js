15894e048eb8999bc9cb3e819330e8c35a5a3d5c
artemkaBionic <artem-morozoff@rambler.ru>
2016-11-11 22:45:07 +0200

    edited

--- suma.js
+++ suma.js
@@ -1,4 +1,3 @@
-url = 'http://bdbdbd.ua/index.html?a=5&b=aa';
 function getAll(url){
     //var query = window.location.search.slice(1);
     var query = url.split('?')[1]; //window.location.search.slice(1);
@@ -8,11 +7,9 @@ function getAll(url){
 
         for (var i=0; i<arr.length; i++) {
             var a = arr[i].split('=');
-            console.log(a);
-
+            
             if(a[0]== 'a' || a[0] == 'b' ){
-var convert = +a[1];
-                console.log(convert);
+                var convert = +a[1];
                 if(!isNaN(convert)){
                     sum=sum + convert;
                 }
